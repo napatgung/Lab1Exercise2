f469ef908a52cfa17c1d99080eb7ac3032bc0b5b
silverwindz <silver9windz@gmail.com>
2015-01-23 15:44:03 +0700

    lab1exercise2

--- app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -14,6 +18,60 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+
+    public void inputTemp(View v)
+    {
+        Double d;
+        String s;
+        EditText etInput = (EditText)findViewById(R.id.etInput);
+
+        s = etInput.getText().toString();
+        d = Double.parseDouble(s);
+
+        RadioGroup rgFrom = (RadioGroup)findViewById(R.id.rgFrom);
+        RadioGroup rgTo = (RadioGroup)findViewById(R.id.rgTo);
+        int selForm = rgFrom.getCheckedRadioButtonId();
+        int selTo = rgTo.getCheckedRadioButtonId();
+        if (selForm == R.id.rbFrmC) { //The user wants to convert from Celsius.
+
+
+        }
+        else if (selForm == R.id.rbFrmF) { //The user wants to convert from Celsius.
+            d = d-32;
+            d=d*5;
+            d =d/9;
+        }
+        else if (selForm == R.id.rbFrmK) { //The user wants to convert from Celsius.
+            d = d-273;
+        }
+
+
+
+
+        if (selTo == R.id.rbToC) { //The user wants to convert from Celsius.
+
+
+        }
+
+        else if (selTo == R.id.rbToF) { //The user wants to convert from Celsius.
+            d = ((9.0/5.0)*d)+32;
+        }
+        else if (selTo == R.id.rbToK) { //The user wants to convert from Celsius.
+            d = d+273;
+        }
+
+        TextView m = (TextView)findViewById(R.id.tvOutput);
+        String n = d.toString();
+        m.setText(n);
+
+
+    }
+//    public void buttonClicked(View v)
+//    {
+//        TextView tv = (TextView)findViewById(R.id.textview);
+//        Double.parseDouble(s);
+//
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
